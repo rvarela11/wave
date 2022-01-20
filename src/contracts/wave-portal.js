4633f7bd7285cab6de019f3c565361f79fc1c2ac
Robert Varela <robertvarela@9eyiwe14keax.wpeproxy.com>
2022-01-19 19:42:27 -0600

    Added wave-portal contract

---
+++ src/contracts/wave-portal.js
@@ -0,0 +1,11 @@
+import { ethers } from "ethers";
+import abi from '../utils/WavePortal.json';
+
+const contractAddress = '0xf34C59497C3E0981938DfB10AD097a753d84c4dd';
+const contractABI = abi.abi;
+
+export const getWavePortalContract = (ethereum) => {
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  return new ethers.Contract(contractAddress, contractABI, signer);
+}
