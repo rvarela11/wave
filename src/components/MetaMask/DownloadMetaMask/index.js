ee8019080dd2242924ccfc03f626de8dda051e66
Robert Varela <robertvarela@9eyiwe14keax.wpeproxy.com>
2022-01-20 22:02:59 -0600

    Added styling to meata mask download

--- src/components/MetaMask/DownloadMetaMask/index.js
+++ src/components/MetaMask/DownloadMetaMask/index.js
@@ -12,11 +12,9 @@ const DownloadMetaMask = () => (
     <div className="download-metaMask">
         <Typography component="h5" variant="h5">MetaMask is needed for this website</Typography>
         <Button href="https://metamask.io/download" target="_blank" variant="contained">Install MetaMask</Button>
-        <img
-            alt="A screenshot of the MetaMask browser Extension"
-            className="download-metaMask__image"
-            src="https://images.ctfassets.net/9sy2a0egs6zh/6ngCUoU36ABPjs6cDNnuoK/a4b9e978595248dbb685aa2c53e3f4dc/download-extension.png"
-        />
+        <div className="download-metaMask__image">
+            <img alt="A screenshot of the MetaMask browser Extension" src="https://images.ctfassets.net/9sy2a0egs6zh/6ngCUoU36ABPjs6cDNnuoK/a4b9e978595248dbb685aa2c53e3f4dc/download-extension.png" />
+        </div>
     </div>
 );
 
