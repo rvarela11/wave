eaa8114f963c8419a2163b91ae79e748761b9247
Robert Varela <rvarela@utexas.edu>
2022-01-28 17:56:44 -0600

    Added logo to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,6 +9,7 @@ import './style.css';
 
 const Header = () => (
     <header>
+        <img src={`${process.env.PUBLIC_URL}/images/wave-logo-icon.png`} />
         <Typography align="center" component="h1" variant="h2">Wave</Typography>
     </header>
 );
